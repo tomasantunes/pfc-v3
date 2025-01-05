6c372866561a1e5734680051c7dd34bc12fc9f2f
Tomás Antunes <tomasantunes@gmail.com>
2025-01-05 15:04:27 +0000

    Add Net Worth value.

--- app.js
+++ app.js
@@ -298,6 +298,48 @@ app.post("/insert-portfolio-snapshot-binance", async (req, res) => {
   res.json({status: "OK", data: "Portfolio snapshot has been inserted successfully."});
 });
 
+app.get("/get-net-worth", async (req, res) => {
+  if (!req.session.isLoggedIn) {
+    res.json({status: "NOK", error: "Invalid Authorization."});
+    return;
+  }
+
+  var sql1 = "SELECT * FROM bpi_mov ORDER BY data_mov DESC, id DESC LIMIT 1";
+  var result1 = await con2.query(sql1);
+  var saldo_bpi = 0;
+  if (result1[0].length > 0) {
+    saldo_bpi = result1[0][0].saldo;
+  }
+  
+
+  var sql2 = "SELECT * FROM t212_portfolio_snapshot_headers ORDER BY created_at DESC LIMIT 1";
+  var result2 = await con2.query(sql2);
+  var saldo_t212 = 0;
+  if (result2[0].length > 0) {
+    saldo_t212 = result2[0][0].balance;
+  }
+  
+
+  var sql3 = "SELECT * FROM coinbase_portfolio_snapshot_headers ORDER BY created_at DESC LIMIT 1";
+  var result3 = await con2.query(sql3);
+  var saldo_coinbase = 0;
+  if (result3[0].length > 0) {
+    saldo_coinbase = result3[0][0].balance;
+  }
+
+  var sql4 = "SELECT * FROM binance_portfolio_snapshot_headers ORDER BY created_at DESC LIMIT 1";
+  var result4 = await con2.query(sql4);
+  var saldo_binance = 0;
+  if (result4[0].length > 0) {
+    saldo_binance = result4[0][0].balance;
+  }
+
+  var total = (Number(saldo_bpi) + Number(saldo_t212) + Number(saldo_coinbase) + Number(saldo_binance)).toFixed(2);
+
+  res.json({status: "OK", data: total});
+
+});
+
 app.post("/api/check-login", (req, res) => {
   var user = req.body.user;
   var pass = req.body.pass;
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Navbar from './Navbar';
+import axios from 'axios';
+import config from '../config';
+import $ from 'jquery';
+
+window.jQuery = $;
+window.$ = $;
+global.jQuery = $;
+window.bootstrap = require('bootstrap');
+var bootprompt = require('bootprompt');
 
 export default function Home() {
+  const [netWorth, setNetWorth] = useState("");
+
+  function getNetWorth() {
+    axios.get(config.BASE_URL + "/get-net-worth")
+    .then(function(response) {
+      setNetWorth(response.data.data.toString() + "€");
+    })
+    .catch(function(err) {
+      bootprompt.alert("Error: " + err.message);
+    });
+  }
+
+  useEffect(() => {
+    getNetWorth();
+  }, []);
+
   return (
     <>
       <Navbar />
-      <div>Dashboard</div>
+      <div className="container">
+        <h3>Dashboard</h3>
+        <p><b>Net Worth:</b> {netWorth}</p>
+      </div>
+      
     </>
   )
 }
