d8bd2e4aa13bc5360e96dda214a8b43a84d76272
Tomás Antunes <tomasantunes@gmail.com>
2025-08-30 15:40:32 +0100

    Fix-up no text input modal e criação de tabela para gravar os dados estimados.

--- database/create-tables.sql
+++ database/create-tables.sql
@@ -79,6 +79,20 @@ CREATE TABLE santander (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE estimated_data (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    incomePerHour DECIMAL(17, 2) NOT NULL,
+    incomePerDay DECIMAL(17, 2) NOT NULL,
+    incomePerWeek DECIMAL(17, 2) NOT NULL,
+    incomePerMonth DECIMAL(17, 2) NOT NULL,
+    incomePerYear DECIMAL(17, 2) NOT NULL,
+    netSalaryPerMonth DECIMAL(17, 2) NOT NULL,
+    netSalaryPerYear DECIMAL(17, 2) NOT NULL,
+    grossSalaryPerMonth DECIMAL(17, 2) NOT NULL,
+    grossSalaryPerYear DECIMAL(17, 2) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 ALTER TABLE bpi_mov ADD COLUMN is_expense BOOLEAN DEFAULT 0;
 ALTER TABLE polymarket_portfolio_snapshot ADD COLUMN deposit DECIMAL(17, 2) DEFAULT 0;
 ALTER TABLE t212_portfolio_snapshot_positions RENAME COLUMN balance TO value;
--- frontend/src/components/TextInputModal.js
+++ frontend/src/components/TextInputModal.js
@@ -6,15 +6,14 @@ export default function TextInputModal({value, setValue}) {
       <div class="modal-dialog">
           <div class="modal-content">
           <div class="modal-header">
-              <h5 class="modal-title" id="textInputModalLabel">Modal title</h5>
               <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-              <input type="text" className="form-control" value={value} onChange={setValue} />
+              <input type="text" className="form-control" value={value} onChange={setValue} style={{backgroundColor: "#99ccff"}} />
           </div>
           <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
+              <button type="button" class="btn btn-primary">Save</button>
           </div>
           </div>
       </div>
