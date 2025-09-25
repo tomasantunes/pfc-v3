5bdc70607a7ff1d1f8299070cb4ef551ff1bfa5f
Tomás Antunes <tomasantunes@gmail.com>
2025-09-25 07:50:12 +0100

    Define the app as a PWA.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import commonjs from "vite-plugin-commonjs"
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), commonjs()]
+  plugins: [
+    react(), 
+    commonjs(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "PFC3",
+        short_name: "PFC3",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "/icons/apple-touch-icon.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+        ]
+      }
+    })
+  ]
 })
