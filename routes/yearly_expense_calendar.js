9455a53c1e7f062d6fe7b9757483a86a8a52d1c6
Tomás Antunes <tomasantunes@gmail.com>
2025-10-22 21:04:34 +0100

    Update yearly expense calendar.

--- routes/yearly_expense_calendar.js
+++ routes/yearly_expense_calendar.js
@@ -6,11 +6,10 @@ const database = require('../libs/database');
 
 var {con, con2} = database.getMySQLConnections();
 
-router.post("/api/yearly-expense-calendar", (req, res) => {
+router.post("/api/yearly-expense-calendar/add", (req, res) => {
   if (!req.session.isLoggedIn) {
-    res.json({status: "NOK", error: "Invalid Authorization."});
-    return;
-	}
+    return res.status(401).json({status: "NOK", error: "Invalid Authorization."});
+  }
 
 	var yedate = req.body.yedate;
 	var description = req.body.description;
@@ -20,6 +19,23 @@ router.post("/api/yearly-expense-calendar", (req, res) => {
 	con2.query(sql, []);
 
 	res.json({status: 'OK', data: con2[0][insertId]});
+});
+
+router.post("/api/yearly-expense-calendar/list", (req, res) => {
+  if (!req.session.isLoggedIn) {
+    return res.status(401).json({status: "NOK", error: "Invalid Authorization."});
+  }
+
+  var sql = "SELECT * FROM yearly_expense_calendar";
+
+  con.query(sql, function(err, result) {
+    if (err) {
+      console.log("Error fetching data from database");
+      return res.json({status: "NOK", error: "Error fetching data from database."});
+    }
+
+    res.json({status: "OK", data: result});
+  });
 });
 
 module.exports = router;
